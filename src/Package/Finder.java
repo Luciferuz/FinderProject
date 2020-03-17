9db2aca35c3e144adc5f5dbd70aeaacff5e46220
Sergei Anciferov <sergozas@yandex.ru>
2020-03-18 00:56:02 +0300

    Added regex.

---
+++ src/Package/Finder.java
@@ -0,0 +1,25 @@
+package Package;
+import java.io.File;
+import java.util.Objects;
+
+public class Finder {
+    private boolean logicR;
+    private String directory;
+    private String filename;
+
+    public Finder(boolean logicR, String directory, String filename) {
+        this.logicR = logicR;
+        this.directory = directory;
+        this.filename = filename;
+    }
+
+    public void find() {
+        File dir = new File(directory);
+        for(File item : Objects.requireNonNull(dir.listFiles())){
+            System.out.println(item.getName() + "  \t folder");
+        }
+    }
+
+}
+
+//find -r -d Users/Admin/ files
--- src/Package/Main.java
+++ src/Package/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_YELLOW = "\u001B[33m"; //для цвета в консоли
-    public static boolean logicR;
+    public static boolean logicR = false;
     public static String directory;
     public static String filename;
 
@@ -14,6 +14,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String userCommand = scanner.nextLine();
         if (!Pattern.matches("find(\\s-r)?\\s(-d)\\s([A-z](/)?)+\\s(.)+", userCommand)) System.out.println("Incorrect input");
-        if (Pattern.matches("find(\\s-r)?\\s(-d)\\s([A-z](/)?)+\\s(.)+", userCommand)) logicR = true;
+
+        Pattern pattern = Pattern.compile("\\s"); //разбивка по пробелу
+        String[] parts = pattern.split(userCommand);
+        if (parts[1].equals("-r")) logicR = true;
+        directory = parts[3];
+        filename = parts[4];
+
+        Finder whatToFind = new Finder(logicR, directory, filename);
+        whatToFind.find();
     }
 }
